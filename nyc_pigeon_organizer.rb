0a7ea7e96eebe9359ca61b27f372da04072495ac
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-16 17:17:29 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,23 +15,5 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-      puts return_hash
+       return_hash
 end
-
-nyc_pigeon_organizer({:color => {
-  :purple => ["Theo", "Peter Jr.", "Lucky"],
-  :grey => ["Theo", "Peter Jr.", "Ms. K"],
-  :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-  :brown => ["Queenie", "Alex"]
-},
-:gender => {
-  :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-  :female => ["Queenie", "Ms. K"]
-},
-:lives => {
-  "Subway" => ["Theo", "Queenie"],
-  "Central Park" => ["Alex", "Ms. K", "Lucky"],
-  "Library" => ["Peter Jr."],
-  "City Hall" => ["Andrew"]
-}
-})
