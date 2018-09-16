58b75630fb3a5a95cad0d833915c98b448297e32
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-16 17:16:24 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   return_hash = {}
   data.each do |spec_key , spec_values|
-    color_values.each do | spec_value_key , spec_value_holders |
+    spec_values.each do | spec_value_key , spec_value_holders |
       spec_value_holders.each do |name|
-          if return_hash[name] == nil 
+          if return_hash[name] == nil
             return_hash[name] = {}
           end
           if return_hash[name][spec_key] == nil
